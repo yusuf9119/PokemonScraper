68f6863c7866c6126c490caa4b2107795c657157
Yusuf Ali <yusufa9119@gmail.com>
2023-11-04 13:35:13 +0000

    second

--- webscraper/miniwebscraper.py
+++ webscraper/miniwebscraper.py
@@ -1,5 +1,24 @@
-import requests as re
+import requests
+import time
+from bs4 import BeautifulSoup
 
-response = re.get("https://www.pokemon.com/us")
-html = response.text
-print(html)
+url = "https://www.pokemon.com/us"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+}
+
+try:
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')  # Use 'html.parser' instead of 'html parser'
+        #scraping code here
+    else:
+        print("Failed to retrieve it. Status code:", response.status_code)
+
+except requests.exceptions.RequestException as e:
+    print("An error has occurred:", e)
+
+time.sleep(1)
